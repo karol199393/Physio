ab10b369d5cce48ad91b7bd0aedb03b7dc3910a9
Karol <114170012+karol199393@users.noreply.github.com>
2024-06-01 08:40:05 +0200

    Add progress repair

--- src/main/java/com/example/Physio/entity/User.java
+++ src/main/java/com/example/Physio/entity/User.java
@@ -13,7 +13,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @Column(nullable = false)
     private String username;
